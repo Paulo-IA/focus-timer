5bc60f5c33fa0fbda2d7de4057fb9ad4a3617cae
Paulo <paulor.exe@gmail.com>
2024-02-21 12:39:13 -0300

    fixing timeout acumulation

--- src/focusTimer/actions.js
+++ src/focusTimer/actions.js
@@ -1,11 +1,14 @@
 import state from "./state.js"
 import * as timer from "./timer.js"
 import * as el from "./elements.js"
+import * as sounds from "./sounds.js"
 
 export function toggleRunning() {
     state.isRunning = document.documentElement.classList.toggle('running')
     
     timer.countdown()
+
+    sounds.buttonPressAudio.play()
 }
 
 export function reset() {
@@ -21,4 +24,11 @@ export function set() {
 
 export function toggleMusic() {
     state.isMute = document.documentElement.classList.toggle("music-on")
+
+    if(state.isMute) {
+        sounds.bgAudio.play()
+        return
+    }
+
+    sounds.bgAudio.pause()
 }
--- src/focusTimer/timer.js
+++ src/focusTimer/timer.js
@@ -1,8 +1,11 @@
 import * as el from './elements.js'
 import state from "./state.js";
 import { reset } from "./actions.js"
+import { kitchenTimer } from "./sounds.js"
 
 export function countdown() {
+    clearTimeout(state.countdownId)
+
     if(!state.isRunning) {
         return
     }
@@ -19,12 +22,13 @@ export function countdown() {
 
     if (minutes < 0) {
         reset()
+        kitchenTimer.play()
         return
     }
 
     updateDisplay(minutes, seconds)
 
-    setTimeout(() => countdown(), 1000)
+    state.countdownId = setTimeout(() => countdown(), 1000)
 }
 
 export function updateDisplay(minutes, seconds) {
@@ -32,8 +36,6 @@ export function updateDisplay(minutes, seconds) {
     seconds = seconds ?? state.seconds
     
     el.minutesElement.textContent = String(minutes).padStart(2, "0")
-    // state.minutes = minutes
 
     el.secondsElement.textContent = String(seconds).padStart(2, "0")
-    // state.seconds = seconds
 }
